98493fc070f73ed7baf01fbfd866ca83cdc1a38e
Atharva Devne <126942964+atharvadevne123@users.noreply.github.com>
2026-07-01 06:11:34 -0400

    chore(8/60): initialise config package

---
+++ config/__init__.py
@@ -0,0 +1,6 @@
+"""Configuration package for Reflective Lantern."""
+
+from config.constants import COMMIT_TARGET, HISTORY_DIR, SCRIPTS_DIR
+from config.settings import Settings
+
+__all__ = ["Settings", "COMMIT_TARGET", "HISTORY_DIR", "SCRIPTS_DIR"]
