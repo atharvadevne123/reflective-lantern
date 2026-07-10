9439351e4ec217e1deb6ae511e5f415ef86f70b9
Reflective Lantern <devneatharva@gmail.com>
2026-07-10 03:19:21 +0000

    chore(1/60): initialise volt-cast app package

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
-"""Realty-Edge: real-estate property valuation and investment scoring."""
+"""Volt-Cast: Energy consumption and grid load prediction package."""
 
 __version__ = "1.0.0"
+__author__ = "Reflective Lantern"
