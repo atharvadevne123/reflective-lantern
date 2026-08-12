ee55f76afe12e04972d90e78857d268b636554c5
Reflective Lantern <devneatharva@gmail.com>
2026-08-12 10:48:08 +0000

    feat(price-prophet 3/60): add app package init

---
+++ price-prophet/app/__init__.py
@@ -0,0 +1,16 @@
+"""
+Price-Prophet: ML-powered dynamic pricing system for e-commerce.
+"""
+
+version = "1.0.0"
+
+__all__ = [
+    "config",
+    "data",
+    "models",
+    "pricing",
+    "evaluation",
+    "api",
+    "utils",
+    "cli",
+]
